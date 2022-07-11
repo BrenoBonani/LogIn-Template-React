b6ea9b5d34c5c17b254c8b8c330b350b4176d719
brenobonani <brenobonani@gmail.com>
2022-07-11 14:39:00 -0300

    Updates

---
+++ precache-manifest.151c028597b03b2c862550eff93ac95e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a92bae2611656e03b0ede0dad03e155e",
+    "url": "/LogIn-Template-React/index.html"
+  },
+  {
+    "revision": "036fdb26aa852edf9588",
+    "url": "/LogIn-Template-React/static/js/2.ec4a975e.chunk.js"
+  },
+  {
+    "revision": "afeb0349f2fb25bfee5f",
+    "url": "/LogIn-Template-React/static/js/main.97f73a11.chunk.js"
+  },
+  {
+    "revision": "7c9c4be904d7bb1689eb",
+    "url": "/LogIn-Template-React/static/js/runtime-main.13a352c3.js"
+  }
+]);
